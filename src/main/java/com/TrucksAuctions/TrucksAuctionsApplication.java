a2061f566588eb7664c7d42d23e6fd9cca7167f8
banuGabrielAndrei <banu.gabrielandrei@gmail.com>
2024-07-31 22:44:32 +0300

    TrucksAuctions

---
+++ src/main/java/com/TrucksAuctions/TrucksAuctionsApplication.java
@@ -0,0 +1,13 @@
+package com.TrucksAuctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrucksAuctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrucksAuctionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TrucksAuctions/TrucksAuctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrucksAuctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrucksAuctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
